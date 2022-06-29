0009010cfed51a30e351c03b58fe5b520a92a088
LyudmilaYashenkova <PerfectLyudmila@yandx.ru>
2022-06-29 23:20:18 +0300

    restart

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from decimal import Decimal
 
+def num_format(num_x): # числовой формат:
+    n = Decimal(str(num_x))
+    number = n.quantize(Decimal("1.00"))  # "1.00" - здесь задаём кол-во знаков после запятой
+    formi = '{0:,}'.format(number).replace(',', ' ')  # (',', ' ') - во вторых кавычках ставим нужный разделитель
+    return formi
